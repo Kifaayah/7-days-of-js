498bbec8cbc98dd2b2e0ec09ebeb05dee8a4a956
Kafayat Jimoh <olajumokekifaayah@gmail.com>
2021-06-22 16:02:11 +0100

    my info and  data-type-assignment folder

---
+++ data-type-assignment/data.js
@@ -0,0 +1,30 @@
+// Task 1
+let userName = "Sophia";
+alert (`Hello ${userName}`);
+console.log (`Hello ${3}`);
+console.log(`Hello ${userName} how are you doing today?`);
+
+// The output are as follows:
+// Hello Sophia
+// Hello 3
+// Hello Sophia how are you doing today?
+
+
+
+// Task 2
+let price = prompt("How much is the price of the good you bought?");
+console.log(`The price of the item is: ${price} dollar`);
+
+
+// The type of the price value is a number
+// The price will  be added to 100 and not concatenate because the price is a number and not a string.
+
+
+// Task 3
+let message = (`Although the day was "sunny", Sophie thought it is wise to take John's umbrella`);
+console.log(message);
+
+
+
+
+
---
+++ myinfo/info.js
@@ -0,0 +1,18 @@
+// Second Task
+
+let myName = "Olajumoke";
+console.log(myName);
+
+let gender = "Female";
+console.log(gender);
+
+let tribe = "Yoruba";
+console.log(tribe);
+
+let digit = 2;
+console.log(typeof (digit));
+
+
+
+
+
